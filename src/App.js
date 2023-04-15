0dbea35f54522286cf3395b22a285157e87b841d
Danny Lam <dannny.lam@gmail.com>
2023-04-16 09:31:27 +1000

    Added basic structure with semantic tags

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import Main from "./components/main/main";
+import Nav from "./components/nav/nav";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
