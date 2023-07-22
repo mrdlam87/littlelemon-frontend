b1c9ae3926a4ea2e43125840c94614b2059504a9
Danny Lam <dannny.lam@gmail.com>
2023-07-23 08:52:21 +1000

    Build version

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,4 @@
-import {
-  render,
-  screen,
-  fireEvent,
-  within,
-  waitFor,
-} from "@testing-library/react";
+import { render, screen, fireEvent, within } from "@testing-library/react";
 import { renderHook, act } from "@testing-library/react-hooks";
 import ReservationForm from "./routes/reservations/reservation-form/ReservationForm";
 import {
@@ -13,7 +7,6 @@ import {
 } from "./contexts/reservation.context";
 import { useContext } from "react";
 import { validate } from "./utils/validation";
-import Reservations from "./routes/reservations/reservations";
 
 test("Renders the Reservations heading", () => {
   render(<ReservationForm />);
@@ -172,3 +165,88 @@ test("should return an empty object for valid values", () => {
 
   expect(errors).toEqual({});
 });
+
+// test("should display error messages for invalid form input", () => {
+//   const invalidValues = {
+//     date: "2021-07-15",
+//     time: "14:00",
+//     guests: "",
+//     name: "",
+//     email: "invalidemail",
+//     phone: "12345",
+//   };
+
+//   render(<ReservationForm />);
+
+//   const dateInput = screen.getByLabelText("date-label");
+//   const timeInput = screen.getByLabelText("time-label");
+//   const guestsInput = screen.getByLabelText("guests-label");
+//   const emailInput = screen.getByLabelText("email-label");
+//   const nameInput = screen.getByLabelText("name-label");
+//   const phoneInput = screen.getByLabelText("phone-label");
+//   const submitButton = screen.getByText("Reserve a Table");
+
+//   fireEvent.click(submitButton);
+
+//   expect(screen.getByText("Required")).toBeInTheDocument();
+
+//   fireEvent.change(dateInput, { target: { value: invalidValues.date } });
+//   fireEvent.change(timeInput, { target: { value: invalidValues.time } });
+//   fireEvent.change(guestsInput, { target: { value: invalidValues.guests } });
+//   fireEvent.change(nameInput, { target: { value: invalidValues.name } });
+//   fireEvent.change(emailInput, { target: { value: invalidValues.email } });
+//   fireEvent.change(phoneInput, { target: { value: invalidValues.phone } });
+//   fireEvent.click(submitButton);
+
+//   expect(screen.getByText("Invalid date")).toBeInTheDocument();
+//   expect(screen.getByText("Invalid time")).toBeInTheDocument();
+//   expect(screen.getByText("Invalid email address")).toBeInTheDocument();
+//   expect(
+//     screen.getByText("Invalid phone number (10 digits required)")
+//   ).toBeInTheDocument();
+// });
+
+// jest.mock("react-router-dom", () => ({
+//   ...jest.requireActual("react-router-dom"), // Use the actual react-router-dom module for other hooks
+//   useNavigate: () => jest.fn(), // Mock the useNavigate function to a mock function
+// }));
+
+// test("should submit the form for valid input", async () => {
+//   const validValues = {
+//     date: "2023-07-30",
+//     time: "18:00",
+//     guests: "4",
+//     name: "John Doe",
+//     email: "john@example.com",
+//     phone: "1234567890",
+//   };
+
+//   render(<Reservations />);
+
+//   const dateInput = screen.getByLabelText("date-label");
+//   const timeInput = screen.getByLabelText("time-label");
+//   const guestsInput = screen.getByLabelText("guests-label");
+//   const emailInput = screen.getByLabelText("email-label");
+//   const nameInput = screen.getByLabelText("name-label");
+//   const phoneInput = screen.getByLabelText("phone-label");
+//   const submitButton = screen.getByText("Reserve a Table");
+
+//   fireEvent.change(dateInput, { target: { value: validValues.date } });
+//   fireEvent.change(timeInput, { target: { value: validValues.time } });
+//   fireEvent.change(guestsInput, { target: { value: validValues.guests } });
+//   fireEvent.change(nameInput, { target: { value: validValues.name } });
+//   fireEvent.change(emailInput, { target: { value: validValues.email } });
+//   fireEvent.change(phoneInput, { target: { value: validValues.phone } });
+
+//   fireEvent.click(submitButton);
+
+//   await waitFor(() => {
+//     expect(phoneInput).toHaveValue("");
+//   });
+
+//   expect(dateInput).toHaveValue("dd/mm/yyyy");
+//   expect(timeInput).toHaveValue("");
+//   expect(guestsInput).toHaveValue("");
+//   expect(nameInput).toHaveValue("");
+//   expect(emailInput).toHaveValue("");
+// });
